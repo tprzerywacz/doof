6a4d5b0e2c8af6f00221dbc75393a404fb561912
Tomek Przerywacz <t.przerywacz@gmail.com>
2022-06-19 13:47:05 +0200

    Added successively food thrown page

--- lib/summary.dart
+++ lib/summary.dart
@@ -2,7 +2,9 @@ import 'package:doof_app/widgets/finish_button.dart';
 import 'package:doof_app/widgets/reason.dart';
 import 'package:doof_app/widgets/quantity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'globals.dart' as globals;
+import 'styles.dart';
 
 class Summary extends StatefulWidget {
   const Summary({Key? key}) : super(key: key);
@@ -21,42 +23,96 @@ class _SummaryState extends State<Summary> {
       count += item.quantity;
     }
 
+    return SingleChildScrollView(
+      child: Column(
+        children: [if (count < 1) _emptyGarbageCan() else _garbageCan()],
+      ),
+    );
+  }
+
+  _emptyGarbageCan() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Center(
+          child: Padding(
+            padding: EdgeInsets.all(40.0),
+            child: Text(
+              'Your food has been thrown away successively!',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.normal),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        SvgPicture.asset('assets/successful.svg'),
+        _backHomeButton(),
+      ],
+    );
+  }
+
+  Widget _backHomeButton()
+  {
+    return
+    Padding(
+      padding: const EdgeInsets.only(left:20, right: 20, top: 40),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(customOrange),
+          textStyle: MaterialStateProperty.all(
+            const TextStyle(fontSize: 18),
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(44.0),
+                side: const BorderSide(color: customOrange)),
+          ),
+        ),
+        onPressed: () {
+          Navigator.pushNamed(context, 'Home');
+        },
+        child: const Padding(
+          padding: EdgeInsets.symmetric(vertical: 22.0),
+          child: Text("Go to home page"),
+        ),
+      ),
+    );
+  }
+
+  Widget _garbageCan() {
     refresh() {
-      setState(() {for (var item in globals.summaryItems) {
-      count += item.quantity;
-    }});
+      setState(() {
+        for (var item in globals.summaryItems) {
+          count += item.quantity;
+        }
+      });
     }
 
     return SingleChildScrollView(
       child: Column(
-        children: [
-          SingleChildScrollView(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Text(
+              'Your garbage can',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                const Padding(
-                  padding: EdgeInsets.all(24.0),
-                  child: Text(
-                    'Your garbage can',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Column(
-                    children: [
-                      for (var item in globals.summaryItems)
-                        _getItem(context, item.name, item.quantity)
-                    ],
-                  ),
-                ),
-                //const Spacer(),
-                if (count < 1)
-                  const Text("There is nothing here!")
-                else
-                  FinishButton(count: count, notifyParent: () { refresh(); },),
+              children: [
+                for (var item in globals.summaryItems)
+                  _getItem(context, item.name, item.quantity)
               ],
             ),
+          ),
+          //const Spacer(),
+          FinishButton(
+            count: count,
+            notifyParent: () {
+              refresh();
+            },
           ),
         ],
       ),
